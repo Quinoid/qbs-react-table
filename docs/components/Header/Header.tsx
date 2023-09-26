ed90c0b9f858aa484d643690081346ae90cc6e13
Rajeshbala2020 <rajeshbala.qbs@gmail.com>
2023-09-26 15:56:55 +0530

    update

--- docs/components/Header/Header.tsx
+++ docs/components/Header/Header.tsx
@@ -5,21 +5,22 @@ import HeartIcon from '@rsuite/icons/legacy/HeartO';
 
 const Header = () => {
   return (
-    <Stack className="header" spacing={8}>
-      <a href="https://www.npmjs.com/package/rsuite-table" target="_blank" rel="noreferrer">
-        <img alt="npm" src="https://img.shields.io/npm/v/rsuite-table?style=for-the-badge" />
-      </a>
-      <IconButton
-        icon={<HeartIcon style={{ fontSize: 20 }} color="red" />}
-        href="https://opencollective.com/rsuite"
-        target="_blank"
-      />
-      <IconButton
-        icon={<GithubIcon style={{ fontSize: 20 }} />}
-        href="https://github.com/rsuite/rsuite-table"
-        target="_blank"
-      />
-    </Stack>
+    <></>
+    // <Stack className="header" spacing={8}>
+    //   <a href="https://www.npmjs.com/package/rsuite-table" target="_blank" rel="noreferrer">
+    //     <img alt="npm" src="https://img.shields.io/npm/v/rsuite-table?style=for-the-badge" />
+    //   </a>
+    //   <IconButton
+    //     icon={<HeartIcon style={{ fontSize: 20 }} color="red" />}
+    //     href="https://opencollective.com/rsuite"
+    //     target="_blank"
+    //   />
+    //   <IconButton
+    //     icon={<GithubIcon style={{ fontSize: 20 }} />}
+    //     href="https://github.com/rsuite/rsuite-table"
+    //     target="_blank"
+    //   />
+    // </Stack>
   );
 };
 
